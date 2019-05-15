8bfb8a13f84aee2135a955e9c09a2ad7121dc28c
Jason Valencius Wijaya <jasonwijaya097@gmail.com>
2019-05-15 14:20:13 +0700

    Initial Commit

---
+++ SmartphoneModel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartphoneModel
+//
+//  Created by Jason Valencius Wijaya on 15/05/19.
+//  Copyright © 2019 Jason Valencius Wijaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
